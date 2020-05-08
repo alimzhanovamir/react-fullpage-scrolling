68f0d4d58edb8bdc201382c1c84e568d6d46a2bd
Alimzhanov Amir <alimzhanovamir@mail.ru>
2020-05-08 20:47:35 +0300

    Updates

---
+++ precache-manifest.f2e7e3ab787e8099f47782e4c7e5ce66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8c46aa2e3311c8cc5f8dd831cfab534",
+    "url": "/react-fullpage-scrolling/index.html"
+  },
+  {
+    "revision": "446c85245b37d5494b6f",
+    "url": "/react-fullpage-scrolling/static/css/main.21598242.chunk.css"
+  },
+  {
+    "revision": "fa9747072ba3039e7855",
+    "url": "/react-fullpage-scrolling/static/js/2.c543977d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-fullpage-scrolling/static/js/2.c543977d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "446c85245b37d5494b6f",
+    "url": "/react-fullpage-scrolling/static/js/main.dc56d69a.chunk.js"
+  },
+  {
+    "revision": "b2fcdbb9bf4f7d91590d",
+    "url": "/react-fullpage-scrolling/static/js/runtime-main.51b724ad.js"
+  }
+]);
