98155a3d54d1e56f6c434750e0fc3096c8257318
Alimzhanov Amir <alimzhanovamir@mail.ru>
2020-05-08 23:10:13 +0300

    Add touchevent

--- src/components/slider/slider.jsx
+++ src/components/slider/slider.jsx
@@ -6,6 +6,7 @@ import { Dots } from '../dots/dots';
 export const Slider = () => {
   const slidesCount = 3;
   const sliderInnerRef = useRef(null);
+  const sliderRef = useRef(null);
   
   let wheelScrollCount = 0;
   const scrollDelay = 300;
@@ -24,7 +25,7 @@ export const Slider = () => {
   }
 
   // Колбэк события прокрутки
-  const wheel = e => {
+  const wheel = e => {   
     // Инкременентируем количество прокруток
     wheelScrollCount++;
     
@@ -63,18 +64,64 @@ export const Slider = () => {
     setTransform(slidesCount, id);
   }
 
+
+
+  let touchStart = 0;
+  let touchEnd = 0;
+  let touchTimer;
+
+  const setSlideFromTouch = (touchStart, touchEnd) => {
+    console.log(touchStart, touchEnd);
+
+    if ( touchStart > touchEnd && currentSlide + 1 < slidesCount) {
+      currentSlide = currentSlide + 1
+      // Делаем сброс кол-ва прокруток как только произошло событие переключения
+      wheelScrollCount = 0;
+    }
+    else if ( touchStart < touchEnd && currentSlide > 0 ) {
+      currentSlide = currentSlide - 1
+      // Делаем сброс кол-ва прокруток как только произошло событие переключения
+      wheelScrollCount = 0;
+    }
+
+    // Пееключить слайд
+    setTransform(slidesCount, currentSlide);
+    // Изменить стейт компонента Dots, задать значение текущего слайда
+    childSetState(currentSlide);
+  }
+
+  const touchStartHandler = e => {
+    touchStart = e.touches[0].pageY;
+  }
+
+  const touchMoveHandler = e => {
+    touchEnd = e.changedTouches[0].pageY;
+
+    if ( touchTimer ) {
+      clearTimeout(touchTimer);
+    }
+
+    // Сброс количества прокруток
+    touchTimer = setTimeout( () => {
+      setSlideFromTouch(touchStart, touchEnd);
+    }, scrollDelay);
+
+  }
+
   /* Жизненный цикл "монтирован"
    * Добавляем слушатель на window
    */ 
   useEffect(() => {
     window.addEventListener('wheel', wheel);
+    sliderRef.current.addEventListener('touchstart', touchStartHandler);
+    sliderRef.current.addEventListener('touchmove', touchMoveHandler);
     return () => window.removeEventListener('wheel', wheel);
   },[]);
 
   return (
     <>
       {console.log('render')}
-      <div className={cls['slider']}>
+      <div className={cls['slider']} ref={sliderRef}>
         <div
           className={cls['slider__inner']}
           ref={sliderInnerRef}>
