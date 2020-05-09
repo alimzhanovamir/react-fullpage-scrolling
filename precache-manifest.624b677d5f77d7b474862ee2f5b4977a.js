5b2f8e84565883a94a5d4f6fa68df92103410c85
Alimzhanov Amir <alimzhanovamir@mail.ru>
2020-05-09 19:03:36 +0300

    Updates

---
+++ precache-manifest.624b677d5f77d7b474862ee2f5b4977a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cab5463e0879de3336e329274151e27",
+    "url": "/react-fullpage-scrolling/index.html"
+  },
+  {
+    "revision": "d1c4324594421e7d14b1",
+    "url": "/react-fullpage-scrolling/static/css/main.a3983bbe.chunk.css"
+  },
+  {
+    "revision": "dcdd69896d7944327fcf",
+    "url": "/react-fullpage-scrolling/static/js/2.917246aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-fullpage-scrolling/static/js/2.917246aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1c4324594421e7d14b1",
+    "url": "/react-fullpage-scrolling/static/js/main.b1917569.chunk.js"
+  },
+  {
+    "revision": "b2fcdbb9bf4f7d91590d",
+    "url": "/react-fullpage-scrolling/static/js/runtime-main.51b724ad.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-fullpage-scrolling/precache-manifest.03cad7ac328b0c614f6757ff063f1e2a.js"
+  "/react-fullpage-scrolling/precache-manifest.624b677d5f77d7b474862ee2f5b4977a.js"
 );
 
 self.addEventListener('message', (event) => {
