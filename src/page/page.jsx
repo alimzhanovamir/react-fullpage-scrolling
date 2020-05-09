607c66470895efdc89d57f34fbe7c6c38ee2f8bf
Alimzhanov Amir <alimzhanovamir@mail.ru>
2020-05-09 17:22:37 +0300

    Refactoring & add slides data

--- src/page/page.jsx
+++ src/page/page.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import cls from './page.module.scss';
-import { Slider } from '../components/fullpage-slider/fullpage-slider';
+import { Fullpageslider } from '../components/fullpage-slider/fullpage-slider';
+import { Slider } from '../components/slider/slider';
 
 export const Page = () => {
+  const slides = [
+    '1',
+    '2',
+    <Slider items={[]}/>
+  ]
+
+  // console.log(slides);
+  
   return (
-   <div className={cls['page']}>
-     <Slider/>
+    <div className={cls['page']}>
+      {console.log('render page')}
+      <Fullpageslider slides={slides}/>
    </div>
   );
 }
